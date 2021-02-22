f7592330765769ac4920afc09729a355dae842b1
Thijs Boom <twboom@hotmail.nl>
2021-02-22 13:04:04 +0100

    Removing linebreaks

--- radon/js/analyzer.js
+++ radon/js/analyzer.js
@@ -1,6 +1,6 @@
 function analyze(text) {
     let count = {};
-    const letters = text.toLowerCase().normalize().trim().split('');
+    const letters = text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace( /[\r\n]+/gm, "" ).trim().split('');
     console.log('Analyzing ' + letters.length + ' letters');
 
     for (let i = 0; i < letters.length; i++) {
